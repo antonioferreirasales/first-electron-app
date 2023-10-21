a7e0338981a0004d80a2a1e78444aeab48feb801
Antônio Sales <antoniosales1003@gmail.com>
2023-10-21 08:31:58 -0300

    feat: the first 2 parts

---
+++ main.js
@@ -0,0 +1,28 @@
+// app, which controls your application's event lifecycle
+// BrowserWindow, which creates and manages app windows
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
